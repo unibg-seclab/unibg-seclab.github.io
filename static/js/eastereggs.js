9302dd2989fa029d8cafbfb3f26ed96e44ad708f
Michele Beretta <beretta.m98@gmail.com>
2025-05-13 18:48:51 +0200

    Host the images ourselves

--- static/js/eastereggs.js
+++ static/js/eastereggs.js
@@ -1,13 +1,7 @@
 let lastClickedImage = null
 let counter = 0
 
-const altimage = {
-  marco: 'https://media.tenor.com/zQPPt2i1yoMAAAAC/wa.gif',
-  matthew: 'https://c.tenor.com/3kPGZrOLeiUAAAAC/pepe-the-frog-dancing.gif',
-  dario: 'https://gifdb.com/images/high/pepe-the-frog-chicken-nuggets-meme-nu5d06wi9gfcbzzf.gif',
-  michele: 'https://media.tenor.com/czUWM5TqIPgAAAAM/2omb-pray.gif',
-  // Professor 'https://gifdb.com/images/high/pepe-the-frog-performing-magic-meme-zlbba8xi5ru2nr7w.gif',
-}
+const hasAlternate = ['marco', 'matthew', 'dario', 'michele']
 
 function multiClick(element) {
   if (element.src !== lastClickedImage) {
@@ -17,9 +11,9 @@ function multiClick(element) {
 
   counter++
   if (counter === 10) {
-    for (key in altimage) {
-      if (element.src.includes(key)) {
-        element.src = altimage[key]
+    for (person of hasAlternate) {
+      if (element.src.includes(person)) {
+        element.src = element.src.replaceAll('image.jpg', 'alternate.gif')
         break
       }
     }
